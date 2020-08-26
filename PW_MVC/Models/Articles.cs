702eb36f018122a75aecbf76e3d5dd791f6b97ef
emamzadehm <emamzadehm@gmail.com>
2020-08-26 19:15:34 +0430

    Partial view

---
+++ PW_MVC/Models/Articles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PW_MVC.Models
+{
+    public class Articles
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+    }
+}
---
+++ PW_MVC/Models/Projects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PW_MVC.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+        public string Img { get; set; }
+    }
+}

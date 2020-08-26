30f746c70849e3b74cd429bba4f8146403a99012
emamzadehm <emamzadehm@gmail.com>
2020-08-26 16:12:20 +0430

    Assets move success

--- PW_MVC/Controllers/HomeController.cs
+++ PW_MVC/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace PW_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
